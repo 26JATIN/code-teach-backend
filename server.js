15a112cfa8d3ded9306416fd6561f8bd4fc9ad05
Jatin Gupta <26jatingupta@gmail.com>
2024-12-20 02:08:31 +0530

    added vercel support

--- server.js
+++ server.js
@@ -7,13 +7,12 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:3000', // Your React app's URL
+  origin: 'https://code-teach.vercel.app', // Your React app's URL
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(express.json());
-
 // MongoDB Connection
 const connectDB = async () => {
   try {
